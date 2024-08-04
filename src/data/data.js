3e0d0717331889b649224fadaf26dc00a3a251d0
parthkorat1 <142651354+parthkorat1@users.noreply.github.com>
2024-08-04 21:13:16 +0530

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/VkRWdHWx/57214s81.jpg',
+    title: "video Streaming App",
+    desc: "",
+    link: "https://github.com/parthkorat1/video-stream",
+    git: "https://github.com/parthkorat1/video-stream",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/Bv75CLyN/4167022.jpg',
+    title: "AI CHAT",
+    link: "https://github.com/parthkorat1/AI-chat",
+    git: "https://github.com/parthkorat1/AI-chat",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/wBjcJcW7/5890625.jpg',
+    title: "E-Commerce",
+    desc: "",
+    link: "",
+    git: "",
+  }
+  
+];
